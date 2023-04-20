8a8a63f201edf7aacdab5357094cf53c9bf3d740
Sergei Morozov <moroz051091@gmail.com>
2023-04-20 12:33:52 +0400

    lesson 1.4

---
+++ lesson01/task04.py
@@ -0,0 +1,8 @@
+val = input(f'Enter positive integer: ')
+i = 0
+val_max = 0
+while i < len(val):
+    if val_max < int(val[i]):
+        val_max = int(val[i])
+    i = i + 1
+print(f'Largest number: {val_max}')
