d1ccdcc05d94d04985e4ed53919a5c35ca158462
Sergei Morozov <moroz051091@gmail.com>
2023-04-26 15:17:31 +0400

    lesson 3.2

---
+++ lesson03/task02.py
@@ -0,0 +1,5 @@
+def data(name, surname, birth_year, city, email, tel):
+    print(f'{name} {surname}, {birth_year}, {city} {email} {tel}')
+
+
+data(name='Sergei', surname='Morozov', birth_year='1991', city='Tbilisi', email='no', tel='no')
